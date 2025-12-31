409290be70da3d7159ada8e9d70d96be445a6274
Utkrisht Jalan <utkrishtjalan5@gmail.com>
2025-12-31 11:13:56 +0530

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,10 +1,11 @@
 import whisper
 
-model = whisper.load_model("base")
+model = whisper.load_model("medium")
 
 def speech_to_text():
-    result = model.transcribe("../input/input.wav", language="en")
-    return result["text"]
+    result = model.transcribe("../input/input.wav")  # AUTO detect
+    return result["text"], result["language"]
 
 if __name__ == "__main__":
-    print(speech_to_text())
+    text, lang = speech_to_text()
+    print(text, lang)
